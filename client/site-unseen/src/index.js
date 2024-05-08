705e9ed550d8ff8fc1a27344a410f13d9b2d5dbf
Justin Pinero <jpinero@resim.ai>
2024-05-08 14:06:00 -0400

    fixing deployment scripts

--- client/site-unseen/src/index.js
+++ client/site-unseen/src/index.js
@@ -1,8 +1,12 @@
 /* REACT */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+/* AXIOS */
+import axios from 'axios';
 /* STYLES */
 import './index.css';
+// const loc = window.location;
+// axios.defaults.baseURL = `${loc.protocol}//${loc.hostname}${loc.hostname === 'localhost' ? ':8080' : ''}`
 /* CUSTOM COMPONENTS */
 import App from './App';
 import reportWebVitals from './reportWebVitals';
