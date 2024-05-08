7d47abeafcfd51015964b08fed03101829c14a72
Justin Pinero <jpinero@resim.ai>
2024-05-08 14:42:21 -0400

    testing render'

--- client/site-unseen/src/index.js
+++ client/site-unseen/src/index.js
@@ -14,7 +14,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <div>
+    TESTING TO SEE HOW MUCH THE MEMORY LEAK IS KILLING THE APP
+   </div>
   </React.StrictMode>
 );
 reportWebVitals();
