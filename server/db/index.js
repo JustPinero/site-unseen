6b0c209bc4d2383883dc0997f88619a2bec94035
Justin Pinero <jpinero@resim.ai>
2024-05-12 22:52:59 -0400

    added sub demographics to sim results

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,10 @@
 const Pool = require("pg").Pool;
 require('dotenv').config() // to use .env variables
 const connectionString = process.env.HEROKU_POSTGRESQL_ORANGE_URL || process.env.DATABASE_URL
-
 const pool = new Pool({connectionString,
+    ssl: {
+        rejectUnauthorized: false
+      }
 });
 
 module.exports = {
